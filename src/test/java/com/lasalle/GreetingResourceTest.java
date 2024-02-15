c5b18fee2755997aff1fc16128dfc54f2e097c36
Wilder Hernández <wihernandez@technisys.com>
2024-02-15 13:20:25 +0100

    First commit

---
+++ src/test/java/com/lasalle/GreetingResourceTest.java
@@ -0,0 +1,16 @@
+package com.lasalle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class GreetingResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        GreetingResource resource = new GreetingResource();
+        String result = resource.hello();
+        assertThat(result, is("Hello World"));
+    }
+
+}
