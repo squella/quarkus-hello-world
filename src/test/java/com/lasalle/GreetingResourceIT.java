1da1a05e4caec96935263ade093d2f8952731649
alejandro <asquella@alejandros-MacBook-Pro.local>
2024-02-22 19:23:09 +0100

    test

--- src/test/java/com/lasalle/GreetingResourceIT.java
+++ src/test/java/com/lasalle/GreetingResourceIT.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
 class GreetingResourceIT {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("http://aeaf7296a50444e25b82e4145761ef25-13049291.eu-west-1.elb.amazonaws.com/hello")
           .then()
              .statusCode(200)
              .body(is("Hello World V2"));
