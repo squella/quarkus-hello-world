5eaff03f52947c409627e16c5fef2059cc4cc664
Wilder Hernández <wihernandez@technisys.com>
2024-02-22 21:21:07 +0100

    fix pipeline

--- testing/tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('http://aeaf7296a50444e25b82e4145761ef25-13049291.eu-west-1.elb.amazonaws.com/hello');
-
-});
